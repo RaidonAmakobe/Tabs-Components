0880a85b89f67000de5b7542861c0e39c3faf38a
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-11 19:08:28 -0400

    Completed Tab.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -19,18 +19,16 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
-
+    const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
-
+    Array.from(links).forEach((links) => {links.removeClass('.tabs-link-selected')});
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
-    
+    this.element = element;
+    element.classList.add('.tabs-link-selected');
     // Call the select method on the item associated with this link
-
   }
 }
+TabLink.select()
 
 class TabItem {
   constructor(element) {
@@ -45,6 +43,7 @@ class TabItem {
     tabItems.removeClass('.tabs-item-selected');
     // Add a class named "tabs-item-selected" to this element
     this.element = element;
+    element.classList.add('.tabs-item-selected');
   }
 }
 
